6c0d81f0f13cc0b2af088283e43b18536c955c41
plspolice <you@example.com>
2015-11-21 15:07:08 +0200

    Add ua

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  private
+  def set_locale
+	  I18n.locale = params[:locale] if params[:locale].present?
+	  # current_user.locale
+	  # request.subdomain
+	  # request.env["HTTP_ACCEPT_LANGUAGE"]
+	  # request.remote_ip
+  end
 end
