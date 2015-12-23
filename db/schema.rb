8a215fa6a27b3068b269a34a54e817dfc638be01
StanisLoveSid <joingamings@gmail.com>
2015-12-23 10:46:17 +0300

    Apache, google, yandex indexing release

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20151203203513) do
     t.string   "name"
     t.text     "body"
     t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
@@ -31,13 +31,6 @@ ActiveRecord::Schema.define(version: 20151203203513) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "servers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
